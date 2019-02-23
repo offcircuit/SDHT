7fbae9293106db905fc6044f5835a7765f33ef9b
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-02-23 23:01:29 +0000

    Update SDHT.cpp

--- SDHT.cpp
+++ SDHT.cpp
@@ -1,21 +1,25 @@
 #include "SDHT.h"
 
 SDHT::SDHT(byte model, byte pin) {
-  _notice = reading(model, pin);
+  _notice = read(model, pin);
   switch (model) {
     case DHT11:
       _humidity = (buffer[0] &= 0x3F);
       _temperature = (buffer[2] &= 0x3F);
       break;
 
-    default:
-       _humidity = word((buffer[0] &= 0x03), buffer[1]) * 0.1;
+    case DHT22:
+      _humidity = word((buffer[0] &= 0x03), buffer[1]) * 0.1;
       _temperature = (word((buffer[2] &= 0x83) & 0x7F, buffer[3]) * 0.1) * (1 - (2 * (buffer[2] & 0x80)));
+      break;
+
+    default:
+      _notice = SDHT_ERROR_MODEL;
   }
-  if (buffer[4] != (buffer[0] + buffer[1] + buffer[2] + buffer[3])) _notice = DHTLIB_ERROR_CHECKSUM;
+  if (buffer[4] != (buffer[0] + buffer[1] + buffer[2] + buffer[3])) _notice = SDHT_ERROR_CHECKSUM;
 }
 
-byte SDHT::reading(byte model, byte pin)
+byte SDHT::read(byte model, byte pin)
 {
   uint8_t mask = 128;
   uint8_t idx = 0;
@@ -23,7 +27,7 @@ byte SDHT::reading(byte model, byte pin)
   uint8_t data = 0;
   uint8_t state = LOW;
   uint8_t pstate = LOW;
-  uint16_t zeroLoop = DHTLIB_TIMEOUT;
+  uint16_t zeroLoop = SDHT_TIMEOUT;
   uint16_t delta = 0;
 
   uint8_t bit = digitalPinToBitMask(pin);
@@ -31,37 +35,35 @@ byte SDHT::reading(byte model, byte pin)
   volatile uint8_t *PIR = portInputRegister(port);
 
   pinMode(pin, OUTPUT);
-  digitalWrite(pin, LOW); // T-be
+  digitalWrite(pin, LOW);
   delay(1 + (model == 11 * 17));
-  digitalWrite(pin, HIGH); // T-go
+  digitalWrite(pin, HIGH);
   pinMode(pin, INPUT);
 
-  uint16_t loopCount = DHTLIB_TIMEOUT * 2;  // 200uSec max
-  while ((*PIR & bit) != LOW ) if (--loopCount == 0) return DHTLIB_ERROR_CONNECT;
+  uint16_t loopCount = SDHT_TIMEOUT * 2;
+  while ((*PIR & bit) != LOW ) if (--loopCount == 0) return SDHT_ERROR_CONNECT;
 
-  // GET ACKNOWLEDGE or TIMEOUT
-  loopCount = DHTLIB_TIMEOUT;
-  while ((*PIR & bit) == LOW ) if (--loopCount == 0) return DHTLIB_ERROR_ACK_L;
+  loopCount = SDHT_TIMEOUT;
+  while ((*PIR & bit) == LOW ) if (--loopCount == 0) return SDHT_ERROR_ACK_L;
 
-  loopCount = DHTLIB_TIMEOUT;
-  while ((*PIR & bit) != LOW ) if (--loopCount == 0) return DHTLIB_ERROR_ACK_H;
+  loopCount = SDHT_TIMEOUT;
+  while ((*PIR & bit) != LOW ) if (--loopCount == 0) return SDHT_ERROR_ACK_H;
 
-  loopCount = DHTLIB_TIMEOUT;
+  loopCount = SDHT_TIMEOUT;
 
-  // READ THE OUTPUT - 40 buffer => 5 BYTES
   for (uint8_t i = 40; i != 0; )
   {
     state = (*PIR & bit);
     if (state == LOW && pstate != LOW)
     {
-      if (i > (40 - (1 + (model != 11 * 5)))) // DHT22 first 6 buffer are all zero !!   DHT11 only 1
+      if (i > (40 - (1 + (model != 11 * 5))))
       {
         zeroLoop = min(zeroLoop, loopCount);
-        delta = (DHTLIB_TIMEOUT - zeroLoop) / 4;
+        delta = (SDHT_TIMEOUT - zeroLoop) / 4;
       } else if ( loopCount <= (zeroLoop - delta) ) data |= mask;
 
       mask >>= 1;
-      if (mask == 0)   // next byte
+      if (mask == 0)
       {
         mask = 128;
         buffer[idx] = data;
@@ -70,20 +72,13 @@ byte SDHT::reading(byte model, byte pin)
       }
 
       --i;
-
-      // reset timeout flag
-      loopCount = DHTLIB_TIMEOUT;
+      loopCount = SDHT_TIMEOUT;
     }
     pstate = state;
-    // Check timeout
-    if (--loopCount == 0)
-    {
-      return DHTLIB_ERROR_TIMEOUT;
-    }
-
+    if (--loopCount == 0) return SDHT_ERROR_TIMEOUT;
   }
   pinMode(pin, OUTPUT);
   digitalWrite(pin, HIGH);
 
-  return DHTLIB_OK;
+  return SDHT_OK;
 }
