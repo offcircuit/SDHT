c6eabcab8c92889dcac86c685034e9337313cf52
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-02-26 22:03:43 +0000

    Add files via upload

--- SDHT.cpp
+++ SDHT.cpp
@@ -22,28 +22,37 @@ uint8_t SDHT::broadcast(uint8_t pin, uint8_t model) {
         _temperature.setCelsius(word((data[2] & 0x7F), data[3]) * ((data[2] & 0x80) ? -.1 : .1));
         break;
     }
+#ifndef SDHT_NO_HEAT
 
-    double heatIndex = (0.5 * (_temperature.fahrenheit + 61.0 + ((_temperature.fahrenheit - 68.0) * 1.2) + (_humidity * 0.094)));
+#ifndef SDHT_NO_FAHRENHEIT
+    double fahrenheit = _temperature.fahrenheit;
+#else
+    double fahrenheit = _temperature.celsius * 1.8 + 32;
+#endif
+
+    double heatIndex = (0.5 * (fahrenheit + 61.0 + ((fahrenheit - 68.0) * 1.2) + (_humidity * 0.094)));
 
     if (heatIndex > 79) {
       heatIndex =
         - 42.37900000
-        + 02.04901523 * _temperature.fahrenheit
+        + 02.04901523 * fahrenheit
         + 10.14333127 * _humidity
-        - 00.22475541 * _temperature.fahrenheit * _humidity
-        - 00.00683783 * pow(_temperature.fahrenheit, 2)
+        - 00.22475541 * fahrenheit * _humidity
+        - 00.00683783 * pow(fahrenheit, 2)
         - 00.05481717 * pow(_humidity, 2)
-        + 00.00122874 * pow(_temperature.fahrenheit, 2) * _humidity
-        + 00.00085282 * _temperature.fahrenheit * pow(_humidity, 2)
-        - 00.00000199 * pow(_temperature.fahrenheit, 2) * pow(_humidity, 2)
+        + 00.00122874 * pow(fahrenheit, 2) * _humidity
+        + 00.00085282 * fahrenheit * pow(_humidity, 2)
+        - 00.00000199 * pow(fahrenheit, 2) * pow(_humidity, 2)
         ;
 
-      if ((_humidity < 13.0) && (_temperature.fahrenheit >= 80.0) && (_temperature.fahrenheit <= 112.0))
-        heatIndex -= ((13.0 - _humidity) * 0.25) * sqrt((17.0 - abs(_temperature.fahrenheit - 95.0)) * 0.05882);
-      else if ((_humidity > 85.0) && (_temperature.fahrenheit >= 80.0) && (_temperature.fahrenheit <= 87.0))
-        heatIndex += ((_humidity - 85.0) * 0.1) * ((87.0 - _temperature.fahrenheit) * 0.2);
+      if ((_humidity < 13.0) && (fahrenheit >= 80.0) && (fahrenheit <= 112.0))
+        heatIndex -= ((13.0 - _humidity) * 0.25) * sqrt((17.0 - abs(fahrenheit - 95.0)) * 0.05882);
+      else if ((_humidity > 85.0) && (fahrenheit >= 80.0) && (fahrenheit <= 87.0))
+        heatIndex += ((_humidity - 85.0) * 0.1) * ((87.0 - fahrenheit) * 0.2);
     }
     _heat.setFahrenheit(heatIndex);
+
+#endif
     _notice = ((data[4] == uint8_t(data[0] + data[1] + data[2] + data[3])) ? SDHT_OK : SDHT_WRONG_PARITY);
   }
   return _notice;
--- SDHT.h
+++ SDHT.h
@@ -7,6 +7,10 @@
 #include "WProgram.h"
 #endif
 
+//#define SDHT_NO_HEAT //UNCOMMENT TO DISABLE HEAT INDEX
+//#define SDHT_NO_KELVIN //UNCOMMENT TO DISABLE KELVIN 
+//#define SDHT_NO_FAHRENHEIT //UNCOMMENT TO DISABLE FAHRENHEIT
+
 #define DHT11 0
 #define DHT12 1
 #define DHT21 2
@@ -31,35 +35,73 @@ class SDHT
   private:
     class Temperature {
       public:
-        const double &celsius = _celsius, &fahrenheit = _fahrenheit, &kelvin = _kelvin;
+        const double &celsius = _celsius;
+
+#ifndef SDHT_NO_FAHRENHEIT
+        const double &fahrenheit = _fahrenheit;
+#endif
+
+#ifndef SDHT_NO_KELVIN
+        const double &kelvin = _kelvin;
+#endif
 
         setCelsius(double c) {
           _celsius = c;
+
+#ifndef SDHT_NO_FAHRENHEIT
           _fahrenheit = c * 1.8 + 32;
+#endif
+
+#ifndef SDHT_NO_KELVIN
           _kelvin = c + 273.15;
+#endif
         }
 
         setFahrenheit(double f) {
           _celsius = (f - 32) / 1.8;
+
+#ifndef SDHT_NO_FAHRENHEIT
           _fahrenheit = f;
+#endif
+
+#ifndef SDHT_NO_KELVIN
           _kelvin = (f + 459.67) * 5 / 9;
+#endif
         }
 
         setKelvin(double k) {
           _celsius = k - 273.15;
+
+#ifndef SDHT_NO_FAHRENHEIT
           _fahrenheit = k * 9 / 5 - 459.67;
+#endif
+
+#ifndef SDHT_NO_KELVIN
           _kelvin = k;
+#endif
         }
 
       private:
-        double _celsius = 0, _fahrenheit = 32, _kelvin = 273.15;
+        double _celsius = 0;
+
+#ifndef SDHT_NO_FAHRENHEIT
+        double _fahrenheit = 32;
+#endif
+
+#ifndef SDHT_NO_KELVIN
+        double _kelvin = 273.15;
+#endif
     };
 
     double _humidity = 0;
     uint8_t data[5], _notice = SDHT_NULL, _pin, _port, _bit;
     uint16_t _signal;
 
-    Temperature _heat, _temperature;
+#ifndef SDHT_NO_HEAT
+    Temperature _heat;
+#endif
+ 
+    Temperature _temperature;
 
     uint8_t read(uint16_t msDelay);
     bool pulse(uint8_t b);
@@ -67,7 +109,12 @@ class SDHT
   public:
     const double &humidity = _humidity;
     const uint8_t &notice = _notice;
-    const Temperature &heat = _heat, &temperature = _temperature;
+    
+#ifndef SDHT_NO_HEAT
+    const Temperature &heat = _heat;
+#endif
+
+    const Temperature &temperature = _temperature;
 
     explicit SDHT() {};
     uint8_t broadcast(uint8_t pin, uint8_t model);
