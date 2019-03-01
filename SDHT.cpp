b3cffe6cd368ba4f3c1c4bcf064614f8c794efc7
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-03-01 09:56:19 +0000

    Update SDHT.cpp

--- SDHT.cpp
+++ SDHT.cpp
@@ -44,53 +44,53 @@ int8_t SDHT::broadcast(uint8_t model, uint8_t pin) {
     switch (model) {
       case DHT11:
         _humidity = data[0] + (data[1] * .1);
-        _temperature.setCelsius(data[2] + (data[3] * .1));
+        _celsius = data[2] + (data[3] * .1);
         break;
 
       case DHT12:
         _humidity = data[0] + (data[1] * .1);
-        _temperature.setCelsius((data[2] + ((data[3] & 0x7F) * .1)) * ((data[3] & 0x80) ? -1 : 1));
+        _celsius = (data[2] + ((data[3] & 0x7F) * .1)) * ((data[3] & 0x80) ? -1 : 1);
         break;
 
       case DHT21:
       case DHT22:
         _humidity = word(data[0], data[1]) * .1;
-        _temperature.setCelsius(word((data[2] & 0x7F), data[3]) * ((data[2] & 0x80) ? -.1 : .1));
+        _celsius = word((data[2] & 0x7F), data[3]) * ((data[2] & 0x80) ? -.1 : .1);
         break;
     }
-#ifndef SDHT_NO_HEAT
-
-#ifndef SDHT_NO_FAHRENHEIT
-    double fahrenheit = _temperature.fahrenheit;
-#else
-    double fahrenheit = _temperature.celsius * 1.8 + 32;
-#endif
-
-    double heatIndex = (0.5 * (fahrenheit + 61.0 + ((fahrenheit - 68.0) * 1.2) + (_humidity * 0.094)));
-
-    if (heatIndex > 79) {
-      heatIndex =
-        - 42.37900000
-        + 02.04901523 * fahrenheit
-        + 10.14333127 * _humidity
-        - 00.22475541 * fahrenheit * _humidity
-        - 00.00683783 * pow(fahrenheit, 2)
-        - 00.05481717 * pow(_humidity, 2)
-        + 00.00122874 * pow(fahrenheit, 2) * _humidity
-        + 00.00085282 * fahrenheit * pow(_humidity, 2)
-        - 00.00000199 * pow(fahrenheit, 2) * pow(_humidity, 2)
-        ;
-
-      if ((_humidity < 13.0) && (fahrenheit >= 80.0) && (fahrenheit <= 112.0))
-        heatIndex -= ((13.0 - _humidity) * 0.25) * sqrt((17.0 - abs(fahrenheit - 95.0)) * 0.05882);
-      else if ((_humidity > 85.0) && (fahrenheit >= 80.0) && (fahrenheit <= 87.0))
-        heatIndex += ((_humidity - 85.0) * 0.1) * ((87.0 - fahrenheit) * 0.2);
-    }
-    _heat.setFahrenheit(heatIndex);
-
-#endif
   }
   return SDHT_NOTICE_OK;
+}
+
+double SDHT::fahrenheit(double celsius) {
+  return celsius * 1.8 + 32;
+}
+
+double SDHT::heatIndex(double humidity, double celsius) {
+
+  celsius = fahrenheit(celsius);
+  
+  double heatIndex = (0.5 * (celsius + 61.0 + ((celsius - 68.0) * 1.2) + (humidity * 0.094)));
+
+  if (heatIndex > 79) {
+    heatIndex =
+      - 42.37900000
+      + 02.04901523 * celsius
+      + 10.14333127 * humidity
+      - 00.22475541 * celsius * humidity
+      - 00.00683783 * pow(celsius, 2)
+      - 00.05481717 * pow(humidity, 2)
+      + 00.00122874 * pow(celsius, 2) * humidity
+      + 00.00085282 * celsius * pow(humidity, 2)
+      - 00.00000199 * pow(celsius, 2) * pow(humidity, 2)
+      ;
+
+    if ((humidity < 13.0) && (celsius >= 80.0) && (celsius <= 112.0))
+      heatIndex -= ((13.0 - humidity) * 0.25) * sqrt((17.0 - abs(celsius - 95.0)) * 0.05882);
+    else if ((humidity > 85.0) && (celsius >= 80.0) && (celsius <= 87.0))
+      heatIndex += ((humidity - 85.0) * 0.1) * ((87.0 - celsius) * 0.2);
+  }
+  return (heatIndex - 32) / 1.8;
 }
 
 uint16_t SDHT::pulse(uint8_t bitmask) {
