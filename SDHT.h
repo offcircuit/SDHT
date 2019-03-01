a4d45de763985f9464012e33f4ef46d063026e01
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-03-01 10:55:44 +0000

    Update SDHT.h

--- SDHT.h
+++ SDHT.h
@@ -17,7 +17,7 @@
 #ifdef ESP8266
 #define SDHT_NOTICE(i) ({xt_wsr_ps(SDHT_REG_STATE); i;})
 #else
-#define SDHT_NOTICE(i) ({SREG ^= 0x80; i;})
+#define SDHT_NOTICE(i) ({SREG = SDHT_REG_STATE; i;})
 #endif
 
 #define SDHT_OK 1
@@ -46,10 +46,11 @@ class SDHT
     const double &humidity = _humidity, &celsius = _celsius;
 
     explicit SDHT() {};
+    
     int8_t broadcast(uint8_t model, uint8_t pin);
     double fahrenheit(double celsius);
     double heatIndex(double humidity, double celsius);
-  
+    
   private:
     double _humidity = 0, _celsius = 0;
     uint8_t _bitmask, _port;
