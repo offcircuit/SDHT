a5043eda35dc2cd0b3b2304e363340cadec762a6
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-03-19 16:35:51 +0000

    Add files via upload

--- SDHT.h
+++ SDHT.h
@@ -1,12 +1,6 @@
 #ifndef SDHT_H
 #define SDHT_H
 
-#if ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
-
 #define DHT11 0
 #define DHT12 1
 #define DHT21 2
@@ -14,50 +8,59 @@
 
 #define SDHT_CYCLES microsecondsToClockCycles(200)
 
-extern int32_t SDHT_REG_STATE;
-
-#ifdef ESP8266
-#define SDHT_NOTICE(i) ({xt_wsr_ps(SDHT_REG_STATE); i;})
-#else
-#define SDHT_NOTICE(i) ({SREG = SDHT_REG_STATE; i;})
-#endif
-
-#define SDHT_OK 1
-#define SDHT_ERROR_PARITY 0
-#define SDHT_ERROR_MODEL -1
-#define SDHT_ERROR_PIN -2
-#define SDHT_ERROR_CONNECT -3
-#define SDHT_ERROR_REQUEST -4
-#define SDHT_ERROR_RESPONSE -5
-#define SDHT_ERROR_LOW -6
-#define SDHT_ERROR_HIGH -7
-
-#define SDHT_NOTICE_OK SDHT_NOTICE(SDHT_OK)
-#define SDHT_NOTICE_ERROR_PARITY SDHT_NOTICE(SDHT_ERROR_PARITY)
-#define SDHT_NOTICE_ERROR_MODEL SDHT_ERROR_MODEL
-#define SDHT_NOTICE_ERROR_PIN SDHT_ERROR_PIN
-#define SDHT_NOTICE_ERROR_CONNECT SDHT_NOTICE(SDHT_ERROR_CONNECT)
-#define SDHT_NOTICE_ERROR_REQUEST SDHT_NOTICE(SDHT_ERROR_REQUEST)
-#define SDHT_NOTICE_ERROR_RESPONSE SDHT_NOTICE(SDHT_ERROR_RESPONSE)
-#define SDHT_NOTICE_ERROR_LOW(i) SDHT_NOTICE(SDHT_ERROR_LOW - (i * 2))
-#define SDHT_NOTICE_ERROR_HIGH(i) SDHT_NOTICE(SDHT_ERROR_HIGH - (i * 2))
-
 class SDHT
 {
+  private:
+    double _humidity = 0, _celsius = 0;
+
   public:
     const double &humidity = _humidity, &celsius = _celsius;
 
     explicit SDHT() {};
-    
-    int8_t broadcast(uint8_t model, uint8_t pin);
-    double fahrenheit(double celsius);
-    double heatIndex(double humidity, double celsius);
-    
-  private:
-    double _humidity = 0, _celsius = 0;
-    uint8_t _bitmask, _port;
 
-    uint16_t pulse(uint8_t bitmask);
+    bool broadcast(uint8_t model, uint8_t pin) {
+      uint8_t data[5] = {0, 0, 0, 0, 0};
+
+#ifdef ESP8266
+      yield();
+#endif
+
+      pinMode(pin, OUTPUT);
+      digitalWrite(pin, LOW);
+      pulseIn(pin, HIGH, (model < DHT21) ? 20000 : 1000);
+      digitalWrite(pin, HIGH);
+      pinMode(pin, INPUT);
+
+      if (!pulseIn(pin, HIGH, 300)) return false;
+      for (uint8_t i = 0; i < 40; i++) data[i / 8] += data[i / 8] + (pulse(pin, LOW) > pulse(pin, HIGH));
+
+      if (data[4] != uint8_t(data[0] + data[1] + data[2] + data[3])) return false;
+
+      switch (model) {
+        case DHT11:
+          _humidity = data[0] + (data[1] * .1);
+          _celsius = data[2] + (data[3] * .1);
+          break;
+
+        case DHT12:
+          _humidity = data[0] + (data[1] * .1);
+          _celsius = (data[2] + ((data[3] & 0x7F) * .1)) * ((data[3] & 0x80) ? -1 : 1);
+          break;
+
+        case DHT21:
+        case DHT22:
+          _humidity = (uint16_t(data[0] << 8) | data[1]) * .1;
+          _celsius = (uint16_t((data[2] & 0x7F)) | data[3]) * ((data[2] & 0x80) ? -.1 : .1);
+          break;
+      }
+      return true;
+    }
+
+    uint16_t pulse(uint8_t pin, int state) {
+      uint16_t c = SDHT_CYCLES;
+      while ((digitalRead(pin) == state) && c--);
+      return c;
+    }
 };
 
 #endif
