80797deaa7c8c1e33dffa0f49a89470c784818af
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-08-07 13:36:50 +0100

    Create broadcast.ino

---
+++ examples/broadcast/broadcast.ino
@@ -0,0 +1,20 @@
+#include "SDHT.h"
+
+SDHT dht;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (dht.broadcast(DHT22, 2)) layout();
+  delay(2000);
+}
+
+void layout() {
+  Serial.print("   Humdity => ");
+  Serial.println(String(double(dht.humidity) / 10, 1));
+  Serial.print("   Celsius => ");
+  Serial.println(String(double(dht.celsius) / 10, 1));
+}
