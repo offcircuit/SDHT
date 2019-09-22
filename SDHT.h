4fd638a26cb965d3b92b2a1e4f5f5c40af2b2ad3
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-09-22 15:33:45 +0100

    Update SDHT.h

--- SDHT.h
+++ SDHT.h
@@ -1,66 +1,64 @@
-#ifndef LCDIC2_H
-#define LCDIC2_H
+#ifndef SDHT_H
+#define SDHT_H
 
-#include "Wire.h"
+#define DHT11 0
+#define DHT12 1
+#define DHT21 2
+#define DHT22 3
 
-#define LCDIC2_MODE         0b100
-#define LCDIC2_DISPLAY      0b1000
-#define LCDIC2_MOVE         0b10000
-#define LCDIC2_FUNCTION     0b100000
-#define LCDIC2_CGRAM        0b1000000
-#define LCDIC2_DDRAM        0b10000000
+#define SDHT_CYCLES microsecondsToClockCycles(200)
 
-#define LCDIC2_BITS_4       0b0
-#define LCDIC2_BITS_8       0b10000
-
-#define LCDIC2_LINES_1      0b0
-#define LCDIC2_LINES_2      0b1000
-
-#define LCDIC2_DOTS_8       0b0
-#define LCDIC2_DOTS_10      0b100
-
-#define LCDIC2_CURSOR       0b0000
-#define LCDIC2_SHIFT        0b1000
-
-#define LCDIC2_LEFT         0b000
-#define LCDIC2_RIGHT        0b100
-
-#define LCDIC2_DEC          0b0
-#define LCDIC2_INC          0b1
-
-class LCDIC2 {
+class SDHT
+{
   private:
-    bool _blink = false, _cursor = true, _display = true, _gain = LCDIC2_INC, _shift = false;
-    uint8_t _address, _height, _width;
-    bool flag();
-    void wait(uint16_t us);
-    bool write(uint8_t data, uint8_t rs = 0);
-    bool writeCommand(uint8_t registry, uint16_t us = 0);
-    void writeData(uint8_t data, uint8_t rs = 0);
-    void writeHigh(uint8_t data, uint8_t rs = 0);
-    void writeLow(uint8_t data, uint8_t rs = 0);
-    bool writeMessage(uint8_t registry, uint8_t data);
+    uint16_t _humidity = 0;
+    int16_t _celsius = 0;
 
   public:
-    LCDIC2(uint8_t address, uint8_t width, uint8_t height);
-    bool begin();
-    bool backlight(bool state);
-    bool blink(bool state);
-    bool clear();
-    bool cursor(bool state);
-    bool cursor(uint8_t x, uint8_t y);
-    bool cursorLeft();
-    bool cursorRight();
-    bool display(bool state);
-    bool glyph(uint8_t character);
-    bool glyph(uint8_t id, uint8_t map[]);
-    bool home();
-    bool leftToRight();
-    bool moveLeft();
-    bool moveRight();
-    size_t print(String data);
-    bool rightToLeft();
-    bool shift(bool state);
+    const uint16_t &humidity = _humidity;
+    const int16_t &celsius = _celsius;
+
+    explicit SDHT() {};
+
+    bool read(uint8_t model, uint8_t pin) {
+      uint8_t data[5] = {0, 0, 0, 0, 0};
+
+      pinMode(pin, OUTPUT);
+      digitalWrite(pin, LOW);
+      pulseIn(pin, HIGH, (model < DHT21) ? 20000 : 1000);
+      digitalWrite(pin, HIGH);
+      pinMode(pin, INPUT);
+
+      if (!pulseIn(pin, HIGH, 300)) return false;
+      for (uint8_t i = 0; i < 40; i++) data[i / 8] += data[i / 8] + (pulse(pin, LOW) > pulse(pin, HIGH));
+
+      if (data[4] != uint8_t(data[0] + data[1] + data[2] + data[3])) return false;
+
+      switch (model) {
+        case DHT11:
+          _humidity = (data[0] * 10) + data[1];
+          _celsius = (data[2] * 10) + data[3];
+          break;
+
+        case DHT12:
+          _humidity = (data[0] * 10) + data[1];
+          _celsius = ((data[2] * 10) + (data[3] & 0x7F)) * ((data[3] & 0x80) ? -1 : 1);
+          
+        case DHT21:
+        case DHT22:
+          _humidity = (uint16_t(data[0] << 8) | data[1]);
+          _celsius = (uint16_t((data[2] & 0x7F) << 8) | data[3]) * ((data[2] & 0x80) ? -1 : 1);
+          
+        default: return false;
+      }
+      return true;
+    }
+
+    uint16_t pulse(uint8_t pin, int state) {
+      uint16_t cycle = SDHT_CYCLES;
+      while ((digitalRead(pin) == state) && cycle--);
+      return cycle;
+    }
 };
 
 #endif
